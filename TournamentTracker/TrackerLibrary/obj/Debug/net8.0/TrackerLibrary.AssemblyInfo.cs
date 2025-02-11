a83bd0efcfb315d10e0fd23081b6a704e4ad1cba
osanburg <osanburg@gmail.com>
2025-02-11 14:32:42 -0800

    Dapper and App.config

--- TournamentTracker/TrackerLibrary/obj/Debug/net8.0/TrackerLibrary.AssemblyInfo.cs
+++ TournamentTracker/TrackerLibrary/obj/Debug/net8.0/TrackerLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrackerLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ba7762e924f750c2873e19b5610107f103780b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a491d2acd87c11d191d773dce4161edb1f1ca9fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrackerLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrackerLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
