fc1332865d1b86172c4ed12c34421f59229cdf61
osanburg <osanburg@gmail.com>
2025-02-12 11:44:13 -0800

    Create TextConnectorProcessor and enabled saving prizes to a text file

--- TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
+++ TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,15 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFiles = "PrizeModels.csv";
+
         // TODO - make the CreatePrize method actually save to a text file
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // Load the text file and convert the text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFiles.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            // Find the max ID
+            int currentId = 1;
+            
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.Max(p => p.Id);
+            }
+            
+            model.Id = currentId;
+
+            // Add the new record with the new ID (max+1)
+            prizes.Add(model);
+
+            
+            // Convert the prizes to list<string>
+            // Save the list<string> to the text file
+            prizes.SaveToPrizeFile(PrizesFiles);
+
             return model;
         }
     }
